77e229ea73585570f1382cfdfcdf39110465eb36
Nhu L.T. Tran <117901760+tntranloc@users.noreply.github.com>
2024-09-09 11:23:44 +0200

    Update transcriptome_wide_association_study_withGEMMA.sh

--- transcriptome_wide_association_study_withGEMMA.sh
+++ transcriptome_wide_association_study_withGEMMA.sh
@@ -1,5 +1,6 @@
+## Prepare bimbam file for TWAS
 # Make kinship
 gemma -g bimbamfile.txt -p trait.txt -gk 2 -o kinshipname 
   # gk 2 is for making kinship with bimbam input
 # Run TWAS
-gemma -g bimbamfile.txt -p traits.txt -k kinshipname.sXX.txt -lmm 1 -o twas_outputname
+gemma -g bimbamfile.txt -p traits.txt -k kinshipname.sXX.txt -lmm 4 -o twas_outputname
