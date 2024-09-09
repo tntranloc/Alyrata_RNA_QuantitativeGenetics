f271e4fd6d1779b9a8d8c33291c96e747f16b75a
Nhu L.T. Tran <117901760+tntranloc@users.noreply.github.com>
2024-09-09 11:25:52 +0200

    Update transcriptome_wide_association_study_withGEMMA.sh

--- transcriptome_wide_association_study_withGEMMA.sh
+++ transcriptome_wide_association_study_withGEMMA.sh
@@ -1,4 +1,11 @@
 ## Prepare bimbam file for TWAS
+  # first column: gene id
+  # second column: a nucleotide (A f.e.)
+  # third column: a corresponding nucleotide (T f.e.)
+  # fourth column onwards: sample names
+  # save the data with write.table(data, "dataname.txt", sep = ",", quote = F, row.names = F, col.names = F)
+
+  
 # Make kinship
 gemma -g bimbamfile.txt -p trait.txt -gk 2 -o kinshipname 
   # gk 2 is for making kinship with bimbam input
