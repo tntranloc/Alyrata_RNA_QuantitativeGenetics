f4247bb1f68582802c07eb55e34fce1db3b6152f
Nhu L.T. Tran <117901760+tntranloc@users.noreply.github.com>
2025-04-28 10:39:37 +0200

    Update transcriptome_wide_association_study_withGEMMA.sh

--- transcriptome_wide_association_study_withGEMMA.sh
+++ transcriptome_wide_association_study_withGEMMA.sh
@@ -9,9 +9,20 @@
 # Make kinship
 gemma -g bimbamfile.txt -p trait.txt -gk 2 -o kinshipname 
   # gk 2 is for making kinship with bimbam input
+
+# Make kinship with PLINK (highly recommended)
+# VCF was filtered to keep INFO/DP >= 10 && F_MISSING < 0.2 && MAF >= 0.05, as well as biallelic SNPs only
+# bcftools view -v snps -m2 -M2 -i 'INFO/DP >= 10 && F_MISSING < 0.2 && MAF >= 0.05
+plink --vcf gwas_plinkdata --make-bed --out gwas_plinkdata
+plink --bfile gwas_plinkdata_clean --make-rel square 
+  # the plink.rel is the kinship I'm going to use
+
 # Run TWAS
+# if kinship was generated with gemma (which I experienced issues and ambiguity so I don't recommend)
 gemma -g bimbamfile.txt -p traits.txt -k kinshipname.sXX.txt -lmm 4 -o twas_outputname
 
+# if kinship was calculated with plink
+gemma -g bimbamfile.txt -p traits.txt -k plink.rel -lmm 4 -o twas_outputname
 
 ## NOTE
 # SNP QC OPTIONS
@@ -23,4 +34,4 @@ gemma -g bimbamfile.txt -p traits.txt -k kinshipname.sXX.txt -lmm 4 -o twas_outp
 
 # When I do normal GWAS, I leave things as default
 # However, when I run TWAS, since these are not "real" SNPs, to avoid them being filtered out, 
-# I have to add -notsnp flag in both kinship generation and twas running steps
+# I have to add -miss 1 -maf 0 -notsnp flag so nothing is filtered out
